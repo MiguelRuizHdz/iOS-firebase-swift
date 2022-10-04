3cf405cc4f5dd84fcc8bd5e11845fdd1167c5dc7
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-04 14:43:46 -0500

    Autenticación mediante Email + Contraseña

---
+++ iOSFirebaseTutorial/AuthViewController.swift
@@ -0,0 +1,64 @@
+//
+//  AuthViewController.swift
+//  iOSFirebaseTutorial
+//
+//  Created by Miguel Angel Ruiz on 04/10/22.
+//
+
+import UIKit
+import FirebaseAnalytics
+import FirebaseAuth
+
+class AuthViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        title = "Autenticación"
+        
+        // Analytics Event
+        Analytics.logEvent("InitScreen", parameters: ["message":"Integración de Firebase completa"])
+    }
+
+    @IBAction func signUpButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) {
+                (result, error) in
+                
+                if let result = result, error == nil {
+                    self.navigationController?.pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando al usuario", preferredStyle: .alert
+                    )
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func logInButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(with: <#T##FIRAuthCredential#>)(withEmail: email, password: password) {
+                (result, error) in
+                
+                if let result = result, error == nil {
+                    self.navigationController?.pushViewController(HomeViewController(email: result.user.email!, provider: .basic), animated: true)
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "Se ha producido un error registrando al usuario", preferredStyle: .alert
+                    )
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
+
